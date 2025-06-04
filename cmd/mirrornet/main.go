f9838d1bc5315d0dabd49e692b81695cb7e18e10
Bhakti <99724683+BhaktiMore18@users.noreply.github.com>
2025-06-04 11:55:50 +0000

    tested the code by generating key pairs

--- cmd/mirrornet/main.go
+++ cmd/mirrornet/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bhaktimore18/mirrornet/internal/crypto"
+)
 
 func main() {
-	fmt.Println("Mirrornet started!✨")
+	fmt.Println("MirrorNet Identity Booting....")
+
+	identity, err := crypto.GenerateIdentity()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Identity Generated!")
+
+	//saving the identity to disk i.e. inside .mirror folder
+
+	err = identity.SaveIdentity(".mirror_id")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Identity saved to .mirror_id")
+
+	//loading it back to confirm
+	loaded, err := crypto.LoadIdentity(".mirror_id")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Loaded Public key : ", loaded.PublicKey)
 }
